12f254a05c72129d7a5a50897d9595312ef0249b
Ravi Arora <51474437+imraviarora@users.noreply.github.com>
2021-04-02 18:49:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,9 @@
 provider "aws" {
   region = "${var.region}"
-  access_key = "AKIAYMP2EDE4EENPH74I"
-  secret_key = "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"
+  #access_key = "AKIAYMP2EDE4EENPH74I"
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  #secret_key = "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"
 }
 
 resource "aws_instance" "web" {
